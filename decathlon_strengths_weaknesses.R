8b53b00546d3976f712727938c8046ec7c13e3c9
mdestr <manuel.destremon@gmail.com>
2025-04-21 02:32:43 +0200

    Maj decathlon

--- decathlon_strengths_weaknesses.R
+++ decathlon_strengths_weaknesses.R
@@ -55,3 +55,71 @@ print(round(mat.cor, 2))
 mat.spearman <- cor(decathlon[,c("X100m", "Longueur", "Poids", "Hauteur", "X400m", "X110m.haies", "Disque", "Perche", "Javelot", "X1500m", "Classement","Nb.pts")],
                     method = "spearman")
 print(round(mat.spearman, 2))
+
+# Pour tester si une association entre deux variables quantitatives est significative 
+# (et donc qu’il existe un lien entre les deux variables) on commence par vérifier 
+# que l’on peut faire une hypothèse de normalité sur les variables. 
+# Pour cela, on peut visualiser les histogrammes, les diagrammes quantile-quantile ou encore effectuer un test de shapiro-wilk. 
+# On prend comme exemple le couple de variables 100m et 400m.
+#normalité
+
+#histogrammes
+histogram(decathlon$X100m)
+histogram(decathlon$X400m)
+
+#diagramme qqplot
+qqmath(decathlon$X100m)
+qqmath(decathlon$X400m)
+
+#test de shapiro
+shapiro.test(decathlon$X100m)
+shapiro.test(decathlon$X400m)
+
+# La normalité des deux variables étant raisonnable au vu du nombre d’observations, on peut envisager un test de corrélation.
+## test de correlation
+cor.test(decathlon$X100m,decathlon$X400m)
+
+# => il y a une corrélation positive significative entre les performances au 100m et au 400m.
+# Les athlètes qui courent vite le 100m tendent aussi à bien performer sur le 400m (même si ce n’est pas une règle stricte).
+# La corrélation de 0.52 est modérée, donc les deux épreuves sont liées mais pas parfaitement.
+# On rejette clairement l’hypothèse selon laquelle il n’y aurait aucun lien (ρ = 0) entre les deux épreuves (p-value ≈ 0.0005)
+
+# Variables quantitatives et qualitatives
+# On représente la distribution des variables quantitatives en fonction de la variable qualitative 
+# et on calcule le η2 entre ces mêmes variables. 
+# Par la suite, on effectue un test pour déterminer le caractère significatif de la liaison.
+
+# Pour la variables Disque par exemple
+
+## via des histogrammes
+histogram( ~ Disque | Epreuve,
+           data = decathlon,
+           type = "density",
+           col = "lightgreen",
+           ylab = "",
+           nint  =k)
+
+## via des boîtes à moustaches
+Boxplot(Disque~Epreuve, data = decathlon)
+
+# On ne distingue pas vraiment de différence de distribution de la variable quantitative (lancer du disque) 
+# selon les modalités de la variable qualitative (JO et Decastar). 
+# ces graphiques sont construits sur peu d’observations, il est donc attendu 
+# d’observer de petites variations d’une compétition à l’autre.
+
+# la variance de la variable quantitative semble similaire dans les deux groupes.
+# calcul du eta2 entre ces deux variable
+
+## Chargement du package BioStatR
+install.packages("BioStatR")
+library(BioStatR)
+
+## calcul du eta2 entre Disque et Epreuve
+eta2(decathlon$Disque,decathlon$Epreuve)
+
+## test de l'association
+anova(lm(decathlon$Disque~decathlon$Epreuve))
+
+# Le coefficient η2 est très proche de zéro et le test de significativité 
+# ne permet pas de mettre en évidence un lien (p.value > 5%).
+
